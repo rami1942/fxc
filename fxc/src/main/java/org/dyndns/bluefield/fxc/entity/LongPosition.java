ad891dcf0c1578ad8fcf0427a624fd7eb11e69a2
rami1942 <rami1942@bluefield.dyndns.org>
2012-08-14 19:36:16 +0900

    Change LongPosition entity for change table defs.

--- fxc/src/main/java/org/dyndns/bluefield/fxc/entity/LongPosition.java
+++ fxc/src/main/java/org/dyndns/bluefield/fxc/entity/LongPosition.java
@@ -4,16 +4,9 @@ import javax.persistence.Entity;
 
 @Entity(name="long_position")
 public class LongPosition {
-	public Integer id;
 	public Double openPrice;
 	public Integer lots;
-	
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
+
 	public Double getOpenPrice() {
 		return openPrice;
 	}
