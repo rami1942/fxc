e1e89b15a3078b72f560d4f4bfb9eb43b8a0443a
rami1942 <rami1942@bluefield.dyndns.org>
2012-09-07 04:35:16 +0900

    Add UpdatePosition, ClearMark, SetMark

--- dll/fxc/scaffold/scaffold.cpp
+++ dll/fxc/scaffold/scaffold.cpp
@@ -15,16 +15,27 @@ extern int __stdcall GetTrapLots();
 extern int __stdcall SetLongPosition(double *buffer, int *lots);
 extern int __stdcall UpdateShortTrap(double *position);
 extern int __stdcall UpdateLongPosition(double *position, int *lots);
+extern int __stdcall SetMark();
+extern int __stdcall ClearMark();
+extern int __stdcall UpdatePosition(int ticket_no, int magic_no, int pos_type, double open_price, double take_profit, double stop_loss, int swap);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 
 	InitEnv();
 
+	/*
 	double price[] = {83.105, 83.058, 82.735, 82.561, 0};
 	int lots[] = {21000, 14000, 21000, 14000, 0};
 	Connect();
 	UpdateLongPosition(price, lots);
+	Disconnect();
+	*/
+
+	Connect();
+	SetMark();
+	UpdatePosition(10, 10, 1, 80.0, 80.0, 80.0, 10);
+	ClearMark();
 	Disconnect();
 
 	TerminateEnv();
