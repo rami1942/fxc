f6de4a04863d2703b541e0b72c80cdf539f3ca52
rami1942 <rami1942@bluefield.dyndns.org>
2013-07-13 17:03:58 +0900

    DDL変更

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -94,6 +94,20 @@ create table position_history (
   pos_type char(1)
 );
 
+create table toggle_tp_request (
+  ticket_no int primary key,
+  tp_price double not null
+);
+
+alter table position add (
+  disp_pos int not null default 0
+);
+
+
+---
+
+drop trigger position_delete;
+
 delimiter //
 create trigger position_delete after delete on position
 for each row
@@ -105,17 +119,22 @@ begin
 
   insert into history_request (ticket_no, pos_cd) 
     values (OLD.ticket_no, OLD.pos_cd);
+
+--  if OLD.ticket_no = 19719151 then
+--    delete from short_trap where open_price in (98.25, 98.0, 97.75);
+--  end if;
+
 end;//
 delimiter ;
 
-create table toggle_tp_request (
-  ticket_no int primary key,
-  tp_price double not null
-);
+delimiter //
+create trigger position_insert after insert on position
+for each row
+begin
+  if NEW.ticket_no = 19726138 then
+    insert into toggle_tp_request (ticket_no, tp_price) values
+    (19717932, 0), (19716234, 0), (19716016);
+  end if;
 
----
-
-alter table position add (
-  disp_pos int not null default 0
-);
-
+end;//
+delimiter ;
